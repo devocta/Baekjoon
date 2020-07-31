bd6fc03a116c5d715e8c27d7dd3c946feb6482ac
devocta <ysjeong15@gmail.com>
2020-07-31 14:50:07 +0900

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-def p(n):
-    num= set(range(2, n+1))
-    for i in range(2, n+1):
-    	if i in num:
-        	num -= set(range(2*i, n+1, i))           
-    return list(num)
-n=int(input())
-print(p(n))
